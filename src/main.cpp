03e1a0657ac89785c608457a3ec05f8cfe4f557f
Dennis Aldana <dennis.aldana@gmail.com>
2023-05-29 17:08:51 -0600

    add: sdl only example

--- src/main.cpp
+++ src/main.cpp
@@ -1,207 +1,80 @@
-#include <iostream>
-#include <fstream>
-#include <algorithm>
+#include <SDL2/SDL.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
 
-// Struct representing RGB color values
-struct Color
-{
-    unsigned char red;
-    unsigned char blue;
-    unsigned char green;
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
-    Color(unsigned char r, unsigned char b, unsigned char g)
-        : red(r), blue(b), green(g)
-    {
-    }
-};
+int main(int argc, char* args[]) {
 
-// Class representing a renderer with width, height, and framebuffer
-class Renderer
-{
-public:
-    int width;
-    int height;
-    unsigned char* framebuffer;
+    SDL_Init(SDL_INIT_VIDEO);
 
-    Renderer(int w, int h)
-        : width(w), height(h)
-    {
-        framebuffer = new unsigned char[width * height * 3];
-    }
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2 Rotation Demo",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_OPENGL
+    );
 
-    ~Renderer()
-    {
-        delete[] framebuffer;
-    }
-};
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-// Function to clear the renderer's framebuffer with a specified color
-void clear(Renderer* renderer, const Color& color)
-{
-    for (int i = 0; i < renderer->width * renderer->height * 3; i += 3)
-    {
-        renderer->framebuffer[i] = color.blue;
-        renderer->framebuffer[i + 1] = color.green;
-        renderer->framebuffer[i + 2] = color.red;
-    }
-}
+    SDL_Point triangle[4];
+    int centerX = SCREEN_WIDTH / 2;
+    int centerY = SCREEN_HEIGHT / 2;
 
-// Function to draw a point on the renderer's framebuffer at a specified position with a specified color
-void point(Renderer* renderer, const glm::ivec2& point, const Color& color)
-{
-    int pixelIndex = (point.y * renderer->width + point.x) * 3;
-    renderer->framebuffer[pixelIndex] = color.blue;
-    renderer->framebuffer[pixelIndex + 1] = color.green;
-    renderer->framebuffer[pixelIndex + 2] = color.red;
-}
+    glm::vec3 points[3] = { 
+        {centerX - 50, centerY - 50, 0.0f}, 
+        {centerX + 50, centerY - 50, 0.0f}, 
+        {centerX, centerY + 50, 0.0f} 
+    };
 
-// Function to draw a line on the renderer's framebuffer from a start position to an end position with a specified color
-void line(Renderer* renderer, const glm::ivec2& start, const glm::ivec2& end, const Color& color)
-{
-    int dx = end.x - start.x;
-    int dy = end.y - start.y;
+    bool isRunning = true;
+    SDL_Event event;
+    float rotateAngle = 0.0f;
 
-    int steps = std::max(abs(dx), abs(dy));
+    unsigned int lastTime = SDL_GetTicks();
+    unsigned int currentTime;
+    float dT;
 
-    float xIncrement = static_cast<float>(dx) / steps;
-    float yIncrement = static_cast<float>(dy) / steps;
-
-    float x = static_cast<float>(start.x);
-    float y = static_cast<float>(start.y);
-
-    for (int i = 0; i <= steps; ++i)
-    {
-        if (y >= 0 && x >= 0 && y < renderer->height && y < renderer->width) {
-            int pixelIndex = (static_cast<int>(y) * renderer->width + static_cast<int>(x)) * 3;    
-            renderer->framebuffer[pixelIndex] = color.blue;
-            renderer->framebuffer[pixelIndex + 1] = color.green;
-            renderer->framebuffer[pixelIndex + 2] = color.red;
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false;
+            }
         }
 
-        x += xIncrement;
-        y += yIncrement;
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Calculate the deltaTime
+        currentTime = SDL_GetTicks();
+        dT = (currentTime - lastTime) / 1000.0f;  // Time since last frame in seconds
+        lastTime = currentTime;
+
+        // Apply rotation
+        rotateAngle += glm::radians(120.0f) * dT;  // Rotate 120 degrees per second
+        glm::mat4 translationToOrigin = glm::translate(glm::mat4(1.0f), glm::vec3(-centerX, -centerY, 0.0f));
+        glm::mat4 rotationMat = glm::rotate(glm::mat4(1.0f), rotateAngle, glm::vec3(0.0f, 0.0f, 1.0f));
+        glm::mat4 translationToCenter = glm::translate(glm::mat4(1.0f), glm::vec3(centerX, centerY, 0.0f));
+
+        // Rotate and translate each point of the triangle
+        for (int i = 0; i < 3; i++) {
+            glm::vec4 transformedPoint = translationToCenter * rotationMat * translationToOrigin * glm::vec4(points[i], 1.0f);
+            triangle[i].x = transformedPoint.x;
+            triangle[i].y = transformedPoint.y;
+        }
+
+        triangle[3] = triangle[0];  // Close the triangle
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDrawLines(renderer, triangle, 4);  // Render the closed triangle
+        SDL_RenderPresent(renderer);
     }
-}
 
-// Function to write the renderer's framebuffer to a BMP file
-void writeBMP(Renderer* renderer, const std::string& filename)
-{
-    std::ofstream file(filename, std::ios::binary);
-
-    unsigned char fileHeader[14] = {
-        'B', 'M',
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        54, 0, 0, 0
-    };
-
-    unsigned char infoHeader[40] = {
-        40, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        1, 0,
-        24, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0
-    };
-
-    int imageSize = renderer->width * renderer->height * 3;
-    int fileSize = imageSize + sizeof(fileHeader) + sizeof(infoHeader);
-
-    *(int*)&fileHeader[2] = fileSize;
-    *(int*)&fileHeader[10] = sizeof(fileHeader) + sizeof(infoHeader);
-    *(int*)&infoHeader[4] = renderer->width;
-    *(int*)&infoHeader[8] = renderer->height;
-    *(int*)&infoHeader[20] = imageSize;
-
-    file.write(reinterpret_cast<char*>(fileHeader), sizeof(fileHeader));
-    file.write(reinterpret_cast<char*>(infoHeader), sizeof(infoHeader));
-    file.write(reinterpret_cast<char*>(renderer->framebuffer), imageSize);
-
-    file.close();
-}
-
-void render(Renderer* renderer)
-{
-    const Color whiteColor(255, 255, 255);
-    const Color redColor(255, 0, 0);
-
-    clear(renderer, whiteColor);
-
-    int squareSize = 100;  // Size of the square
-
-    glm::ivec2 center(renderer->width / 2, renderer->height / 2);  // Center of the screen
-
-    // Calculate the coordinates of the four corners of the square
-    glm::ivec2 topLeft = center - glm::ivec2(squareSize / 2, squareSize / 2);
-    glm::ivec2 topRight = center + glm::ivec2(squareSize / 2, -squareSize / 2);
-    glm::ivec2 bottomLeft = center + glm::ivec2(-squareSize / 2, squareSize / 2);
-    glm::ivec2 bottomRight = center + glm::ivec2(squareSize / 2, squareSize / 2);
-
-    glm::mat3 translateToCenter = glm::mat3(
-        1, 0, -renderer->width / 2,
-        0, 1, -renderer->height / 2,
-        0, 0, 1
-    );
-
-    glm::mat3 scale = glm::mat3(
-        2, 0, 0,
-        0, 2, 0,
-        0, 0, 1
-    );
-
-    glm::mat3 rotate = glm::mat3(
-        cos(glm::radians(45.0f)), -sin(glm::radians(45.0f)), 0.0f,
-        sin(glm::radians(45.0f)), cos(glm::radians(45.0f)), 0.0f,
-        0.0f, 0.0f, 1.0f
-    );
-
-    glm::mat3 translateBack = glm::mat3(
-        1, 0, renderer->width / 2,
-        0, 1, renderer->height / 2,
-        0, 0, 1
-    );
-
-    glm::mat3 transformMatrix = translateToCenter * rotate * scale * translateBack;
-
-    glm::ivec3 topLeftAugmented(topLeft.x, topLeft.y, 1);
-    glm::ivec3 topRightAugmented(topRight.x, topRight.y, 1);
-    glm::ivec3 bottomLeftAugmented(bottomLeft.x, bottomLeft.y, 1);
-    glm::ivec3 bottomRightAugmented(bottomRight.x, bottomRight.y, 1);
-
-
-    glm::ivec3 transformedTopLeft = topLeftAugmented * transformMatrix;
-    glm::ivec3 transformedTopRight = topRightAugmented * transformMatrix;
-    glm::ivec3 transformedBottomLeft = bottomLeftAugmented * transformMatrix;
-    glm::ivec3 transformedBottomRight = bottomRightAugmented * transformMatrix;
-
-    topLeft = glm::ivec2(transformedTopLeft.x / transformedTopLeft.z, transformedTopLeft.y / transformedTopLeft.z);
-    topRight = glm::ivec2(transformedTopRight.x / transformedTopRight.z, transformedTopRight.y / transformedTopRight.z);
-    bottomLeft = glm::ivec2(transformedBottomLeft.x / transformedBottomLeft.z, transformedBottomLeft.y / transformedBottomLeft.z);
-    bottomRight = glm::ivec2(transformedBottomRight.x / transformedBottomRight.z, transformedBottomRight.y / transformedBottomRight.z);
-
-    line(renderer, topLeft, topRight, redColor);
-    line(renderer, topRight, bottomRight, redColor);
-    line(renderer, bottomRight, bottomLeft, redColor);
-    line(renderer, bottomLeft, topLeft, redColor);
-}
-
-
-int main()
-{
-    const int width = 800;
-    const int height = 600;
-
-    Renderer renderer(width, height);
-
-    render(&renderer);
-
-    writeBMP(&renderer, "image.bmp");
-
-    std::cout << "Image generated successfully!" << std::endl;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }

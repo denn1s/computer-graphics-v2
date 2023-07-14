d7f2dff5a9a8b9b92de445ceb2b7ee5f566443d2
Dennis Aldana <dennis.aldana@gmail.com>
2023-07-14 11:57:52 -0600

    add: base code for game of life

--- src/main.cpp
+++ src/main.cpp
@@ -1,189 +1,116 @@
-#include <iostream>
-#include <fstream>
+#include <SDL2/SDL.h>
 
-// Color struct to represent RGB values
-struct Color
-{
-    unsigned char red;
-    unsigned char green;
-    unsigned char blue;
+// Define the size of the framebuffer
+const int FRAMEBUFFER_WIDTH = 80;
+const int FRAMEBUFFER_HEIGHT = 80;
+const int FRAMEBUFFER_SIZE = FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 800;
 
-    Color(unsigned char r, unsigned char g, unsigned char b)
-        : red(r), green(g), blue(b)
-    {
-    }
+// Define a Color struct to hold the RGB values of a pixel
+struct Color {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
 };
 
-// Function to write BMP image file
-void writeBMP(const std::string& filename, int width, int height, unsigned char* framebuffer)
-{
-    std::ofstream file(filename, std::ios::binary);
+// Declare the framebuffer as a global variable
+Color framebuffer[FRAMEBUFFER_SIZE];
 
-    // BMP file header
-    unsigned char fileHeader[] = {
-        'B', 'M',  // Signature
-        0, 0, 0, 0, // File size (will be filled later)
-        0, 0,      // Reserved
-        0, 0,      // Reserved
-        54, 0, 0, 0 // Pixel data offset
-    };
+// Declare a global clearColor of type Color
+Color clearColor = {0, 0, 0, 255}; // Initially set to black
 
-    // BMP info header
-    unsigned char infoHeader[] = {
-        40, 0, 0, 0,  // Info header size
-        0, 0, 0, 0,   // Image width (will be filled later)
-        0, 0, 0, 0,   // Image height (will be filled later)
-        1, 0,         // Number of color planes
-        24, 0,        // Bits per pixel (3 bytes)
-        0, 0, 0, 0,   // Compression method
-        0, 0, 0, 0,   // Image size (will be filled later)
-        0, 0, 0, 0,   // Horizontal resolution (pixels per meter, not used)
-        0, 0, 0, 0,   // Vertical resolution (pixels per meter, not used)
-        0, 0, 0, 0,   // Number of colors in the palette (not used)
-        0, 0, 0, 0    // Number of important colors (not used)
-    };
+// Declare a global currentColor of type Color
+Color currentColor = {255, 255, 255, 255}; // Initially set to white
 
-    // Calculate some values
-    int imageSize = width * height * 3;  // 3 bytes per pixel (BGR)
-    int fileSize = imageSize + sizeof(fileHeader) + sizeof(infoHeader);
+int x = 10;
+int y = 10;
 
-    // Fill in the file header
-    *(int*)&fileHeader[2] = fileSize;          // File size
-    *(int*)&fileHeader[10] = sizeof(fileHeader) + sizeof(infoHeader);  // Pixel data offset
-
-    // Fill in the info header
-    *(int*)&infoHeader[4] = width;             // Image width
-    *(int*)&infoHeader[8] = height;            // Image height
-    *(int*)&infoHeader[20] = imageSize;        // Image size
-
-    // Write the headers to the file
-    file.write(reinterpret_cast<char*>(fileHeader), sizeof(fileHeader));
-    file.write(reinterpret_cast<char*>(infoHeader), sizeof(infoHeader));
-
-    // Write the pixel data
-    file.write(reinterpret_cast<char*>(framebuffer), imageSize);
-
-    // Close the file
-    file.close();
-}
-
-void clear(unsigned char* framebuffer, int width, int height, const Color& color)
-{
-    for (int i = 0; i < width * height * 3; i += 3)
-    {
-        framebuffer[i] = color.blue;
-        framebuffer[i + 1] = color.green;
-        framebuffer[i + 2] = color.red;
+// Function to clear the framebuffer with the clearColor
+void clear() {
+    for (int i = 0; i < FRAMEBUFFER_SIZE; i++) {
+        framebuffer[i] = clearColor;
     }
 }
 
-void point(unsigned char* framebuffer, int width, int height, int x, int y, const Color& color)
-{
-    int pixelIndex = (y * width + x) * 3;
-    framebuffer[pixelIndex] = color.blue;
-    framebuffer[pixelIndex + 1] = color.green;
-    framebuffer[pixelIndex + 2] = color.red;
-}
-
-void drawGundam(unsigned char* framebuffer, int width, int height)
-{
-    // Draw the head
-    for (int y = 0; y < 100; ++y)
-    {
-        for (int x = 0; x < 100; ++x)
-        {
-            point(framebuffer, width, height, x + 100, height - (y + 100) - 1, Color(0, 0, 0));
-        }
-    }
-
-    // Draw the antennas
-    for (int y = 0; y < 50; ++y)
-    {
-        for (int x = 0; x < 10; ++x)
-        {
-            point(framebuffer, width, height, x + 140, height - (y + 80) - 1, Color(0, 0, 0));
-            point(framebuffer, width, height, x + 160, height - (y + 80) - 1, Color(0, 0, 0));
-        }
-    }
-
-    // Draw the body
-    for (int y = 0; y < 200; ++y)
-    {
-        for (int x = 0; x < 50; ++x)
-        {
-            point(framebuffer, width, height, x + 125, height - (y + 200) - 1, Color(255, 0, 0));
-        }
-    }
-
-    // Draw the legs
-    for (int y = 0; y < 150; ++y)
-    {
-        for (int x = 0; x < 50; ++x)
-        {
-            point(framebuffer, width, height, x + 125, height - (y + 400) - 1, Color(255, 0, 0));
-            point(framebuffer, width, height, x + 225, height - (y + 400) - 1, Color(255, 0, 0));
-        }
-    }
-
-    // Draw the beam saber
-    for (int y = 0; y < 300; ++y)
-    {
-        for (int x = 0; x < 10; ++x)
-        {
-            point(framebuffer, width, height, x + 155, height - (y + 100) - 1, Color(0, 255, 255));
-        }
-    }
-
-    // Draw the shield
-    for (int y = 0; y < 150; ++y)
-    {
-        for (int x = 0; x < 150; ++x)
-        {
-            point(framebuffer, width, height, x + 50, height - (y + 200) - 1, Color(0, 0, 255));
-        }
-    }
-
-    // Draw the funnels
-    for (int y = 0; y < 50; ++y)
-    {
-        for (int x = 0; x < 10; ++x)
-        {
-            point(framebuffer, width, height, x + 50, height - (y + 400) - 1, Color(255, 255, 0));
-            point(framebuffer, width, height, x + 75, height - (y + 400) - 1, Color(255, 255, 0));
-            point(framebuffer, width, height, x + 100, height - (y + 400) - 1, Color(255, 255, 0));
-            point(framebuffer, width, height, x + 125, height - (y + 400) - 1, Color(255, 255, 0));
-            point(framebuffer, width, height, x + 150, height - (y + 400) - 1, Color(255, 255, 0));
-        }
+// Function to set a specific pixel in the framebuffer to the currentColor
+void point(int x, int y) {
+    if (x >= 0 && x < FRAMEBUFFER_WIDTH && y >= 0 && y < FRAMEBUFFER_HEIGHT) {
+        framebuffer[y * FRAMEBUFFER_WIDTH + x] = currentColor;
     }
 }
 
+void renderBuffer(SDL_Renderer* renderer) {
+    // Create a texture
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer, 
+        SDL_PIXELFORMAT_ABGR8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        FRAMEBUFFER_WIDTH, 
+        FRAMEBUFFER_HEIGHT
+    );
 
-int main()
-{
-    const int width = 1900;
-    const int height = 1080;
+    // Update the texture with the pixel data from the framebuffer
+    SDL_UpdateTexture(
+        texture, 
+        NULL, 
+        framebuffer, 
+        FRAMEBUFFER_WIDTH * sizeof(Color)
+    );
 
-    // Create an array to hold the pixel data (framebuffer)
-    unsigned char* framebuffer = new unsigned char[width * height * 3];
+    // Copy the texture to the renderer
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
 
-    // Clear the framebuffer to white (255, 255, 255)
-    clear(framebuffer, width, height, Color(255, 255, 255));
+    // Destroy the texture
+    SDL_DestroyTexture(texture);
+}
 
-    // Calculate the center coordinates
-    int centerX = width / 2;
-    int centerY = height / 2;
+void render(SDL_Renderer* renderer) {
+    // Clear the framebuffer
+    // clear();
 
-    // Set the center pixel to red (0, 0, 255)
-    // point(framebuffer, width, height, centerX, centerY, Color(255, 0, 0));
-    drawGundam(framebuffer, width, height);
-    
-    // Write the image to a BMP file
-    writeBMP("image.bmp", width, height, framebuffer);
+    // Draw a point
+    point(x++, y++);
 
-    // Clean up the memory
-    delete[] framebuffer;
+    // Render the framebuffer to the screen
+    renderBuffer(renderer);
+}
 
-    std::cout << "Image generated successfully!" << std::endl;
+int main() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow("life", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Call our render function
+        render(renderer);
+
+        // Present the frame buffer to the screen
+        SDL_RenderPresent(renderer);
+
+        // Delay to limit the frame rate
+        SDL_Delay(1000 / 60);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }

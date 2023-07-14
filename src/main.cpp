5536d6993a6222593281bbce78fbc3b5eeff0aae
Dennis Aldana <dennis.aldana@gmail.com>
2023-07-14 12:30:07 -0600

    add: sample for game of life

--- src/main.cpp
+++ src/main.cpp
@@ -66,12 +66,122 @@ void renderBuffer(SDL_Renderer* renderer) {
     SDL_DestroyTexture(texture);
 }
 
-void render(SDL_Renderer* renderer) {
+// A temporary framebuffer to hold the next generation
+Color nextFramebuffer[FRAMEBUFFER_SIZE];
+
+void life() {
+    for (int y = 0; y < FRAMEBUFFER_HEIGHT; y++) {
+        for (int x = 0; x < FRAMEBUFFER_WIDTH; x++) {
+            int livingNeighbors = 0;
+
+            // Check the 8 neighboring cells
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dx = -1; dx <= 1; dx++) {
+                    if (dx == 0 && dy == 0) continue;  // Skip the cell itself
+
+                    int nx = x + dx;
+                    int ny = y + dy;
+
+                    // Check the boundaries
+                    if (nx >= 0 && nx < FRAMEBUFFER_WIDTH && ny >= 0 && ny < FRAMEBUFFER_HEIGHT) {
+                        if (framebuffer[ny * FRAMEBUFFER_WIDTH + nx].r == 255) {
+                            livingNeighbors++;
+                        }
+                    }
+                }
+            }
+
+            // Apply the rules of the game
+            if (framebuffer[y * FRAMEBUFFER_WIDTH + x].r == 255) {
+                if (livingNeighbors < 2 || livingNeighbors > 3) {
+                    // Underpopulation or Overpopulation: cell dies
+                    nextFramebuffer[y * FRAMEBUFFER_WIDTH + x] = clearColor;
+                } else {
+                    // Survival: cell stays alive
+                    nextFramebuffer[y * FRAMEBUFFER_WIDTH + x] = currentColor;
+                }
+            } else {
+                if (livingNeighbors == 3) {
+                    // Reproduction: cell becomes alive
+                    nextFramebuffer[y * FRAMEBUFFER_WIDTH + x] = currentColor;
+                } else {
+                    // Cell stays dead
+                    nextFramebuffer[y * FRAMEBUFFER_WIDTH + x] = clearColor;
+                }
+            }
+        }
+    }
+
+    // Update the framebuffer for the next frame
+    memcpy(framebuffer, nextFramebuffer, FRAMEBUFFER_SIZE * sizeof(Color));
+}
+
+// Function to add a glider at a given position
+void glider(int x, int y) {
+    const int DX[5] = {1, 2, 0, 1, 2};
+    const int DY[5] = {0, 0, 1, 1, 2};
+
+    for (int i = 0; i < 5; i++) {
+        int nx = x + DX[i];
+        int ny = y + DY[i];
+
+        // Check the boundaries
+        if (nx >= 0 && nx < FRAMEBUFFER_WIDTH && ny >= 0 && ny < FRAMEBUFFER_HEIGHT) {
+            framebuffer[ny * FRAMEBUFFER_WIDTH + nx] = currentColor;
+        }
+    }
+}
+
+void heavySpaceship(int x, int y) {
+    const int DX[11] = {0, 4, 0, 1, 2, 3, 4, 5, 1, 2, 5};
+    const int DY[11] = {0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2};
+
+    for (int i = 0; i < 11; i++) {
+        int nx = x + DX[i];
+        int ny = y + DY[i];
+
+        // Check the boundaries
+        if (nx >= 0 && nx < FRAMEBUFFER_WIDTH && ny >= 0 && ny < FRAMEBUFFER_HEIGHT) {
+            framebuffer[ny * FRAMEBUFFER_WIDTH + nx] = currentColor;
+        }
+    }
+}
+
+void gosperGliderGun(int x, int y) {
+    const int DX[36] = {1, 3, 0, 1, 0, 1, 14, 15, 13, 17, 12, 18, 12, 18, 19, 11, 21, 22, 23, 24, 35, 36, 22, 24, 12, 16, 13, 17, 14, 15, 0, 1, 2, 7, 3, 7};
+    const int DY[36] = {5, 5, 6, 6, 7, 7, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 4, 4, 5, 5, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 14, 14, 15, 15, 16, 16};
+
+    for (int i = 0; i < 36; i++) {
+        int nx = x + DX[i];
+        int ny = y + DY[i];
+
+        // Check the boundaries
+        if (nx >= 0 && nx < FRAMEBUFFER_WIDTH && ny >= 0 && ny < FRAMEBUFFER_HEIGHT) {
+            framebuffer[ny * FRAMEBUFFER_WIDTH + nx] = currentColor;
+        }
+    }
+}
+
+void initialRender() {
     // Clear the framebuffer
     clear();
 
-    // Draw a point
-    point(x++, y++);
+    // Add some gliders
+    // glider(10, 10);
+    // glider(20, 20);
+    // glider(30, 30);
+
+    // heavySpaceship(40, 40);
+    // heavySpaceship(50, 50);
+
+    gosperGliderGun(10, 10);
+
+    // Add more gliders if you want...
+}
+
+
+void render(SDL_Renderer* renderer) {
+    life();
 
     // Render the framebuffer to the screen
     renderBuffer(renderer);
@@ -90,6 +200,9 @@ int main() {
 
     bool running = true;
     SDL_Event event;
+
+    initialRender();
+
 
     while (running) {
         while (SDL_PollEvent(&event)) {

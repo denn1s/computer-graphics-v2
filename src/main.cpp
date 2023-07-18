77a1158579c0974d377b46dc2e3c5f5519bda6a4
Dennis Aldana <dennis.aldana@gmail.com>
2023-07-17 18:04:05 -0600

    fix: add segfault example

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 int main() {
     short s = 4;
     int i = 5;
-    long l = 6;
     int h = 0x11223344;
 
     short* pointer = &s;
-
-    char* string = "hello";
-
     char* array = new char[4];
     array[3] = 9;
+    array[5] = 8;
+
+
+    const char* string = "hello";
+
 
     return 0;
 }

f5b7545b8a6f40511bc79b3c705987bd42964bc5
Dennis Aldana <dennis.aldana@gmail.com>
2023-07-17 18:13:47 -0600

    fix: add example with manual indexing

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@ int main() {
     int h = 0x11223344;
 
     short* pointer = &s;
-    char* array = new char[4];
-    array[3] = 9;
-    array[5] = 8;
+    short* array = new short[4];
+    *(array + 0) = 2;
+    *(array + 1) = 3;
+    *(array + 2) = 4;
+
+    // array[3] = 9;
+    // array[5] = 8;
 
 
     const char* string = "hello";
